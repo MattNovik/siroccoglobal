bb2e82ddedc8e8ca4b8ff3fe858c62df3410123a
Godicks <matvej.novik@gmail.com>
2022-10-22 08:57:11 +0300

    second init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData:
+          '@import "./src/scss/variables.scss";\n@import "./src/scss/mixins.scss";',
+      },
+    },
+  },
+});
